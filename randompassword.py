d6a7bf89d65363918be157ccf20339b6e89a9cc9
Soham-59 <162586540+Soham-59@users.noreply.github.com>
2024-03-15 20:35:42 +0530

    Add files via upload

---
+++ randompassword.py
@@ -0,0 +1,11 @@
+import random
+import string
+uppercase=("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+lowercase=("abcdefghijklmnopqrstuvwxyz")
+digits=("0123456789")
+symbol=("@!#%*?/")
+
+string=uppercase+lowercase+digits+symbol
+length=int(input("Enter the length of password:"))
+password="".join(random.sample(string,length))    
+print("The Generated Password is:",password)
